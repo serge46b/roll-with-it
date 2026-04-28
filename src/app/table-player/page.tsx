6be40fd377e2da815a81f30c18c35d8e37b848cd
Asya <aneXXa@users.noreply.github.com>
2026-04-28 12:00:29 +0700

    Add PlanetScreen and TablePlayer components; include Plus SVG icon. Setting chouse world or created world

---
+++ src/app/table-player/page.tsx
@@ -0,0 +1,1 @@
+export { default } from "@/modules/TablePlayer/components/page"
---
+++ src/modules/TablePlayer/components/page.tsx
@@ -0,0 +1,28 @@
+import {
+    BlocksPole,
+    ChatPole,
+    Dise,
+    Header,
+    InterfaceHp,
+    SmallInterfaceHp,
+  } from "@/components"
+  
+  export default function PlayPlayer() {
+    return (
+      <div className="relative min-h-screen w-full overflow-hidden">
+        <div className="absolute inset-0 bg-white" />
+        <Header />
+        <ChatPole />
+        <div className="fixed bottom-0 z-20 w-full overflow-x-auto px-2">
+          <div className="mx-auto flex w-full min-w-0 max-w-full flex-row items-end justify-center gap-5">
+            <Dise />
+            <BlocksPole text="Пресонаж" />
+            <BlocksPole text="Оружие" />
+            <BlocksPole text="Заклинания" />
+            <BlocksPole text="Инвентарь" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+``  
