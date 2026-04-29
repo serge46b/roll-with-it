fd58f29138e335558eb81981e346c5d4c331ebe8
Asya <aneXXa@users.noreply.github.com>
2026-04-29 12:00:35 +0700

    Cleanup

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Dise, FunDise, rollDice } from "./Dise"
-export { BlocksPole } from "./FunBlocks"
-export { ChatPole } from "./Chat"
-export { ActionPanelFrame } from "./Frame"
-export { InterfaceHp } from "./InterfaceHp"
-export { SmallInterfaceHp } from "./SmallInterfaceHp"
-export { TopBar as Header } from "@/modules/Topbar"
