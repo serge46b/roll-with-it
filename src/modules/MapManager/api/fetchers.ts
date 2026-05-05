7b2a5929a2b6571034ab27d1b7180af93529c209
Nastya <eshsen@users.noreply.github.com>
2026-05-05 12:00:57 +0700

    Deleted temporary hardcoded numbers and fixed image size after loading

--- src/modules/MapManager/api/fetchers.ts
+++ src/modules/MapManager/api/fetchers.ts
@@ -7,26 +7,27 @@ export async function fetchMapImage(worldUUID: string, mapId: number) {
   const supabase = await createClient()
   const { data: metadata, error: metadataError } = await supabase.storage
     .from("ImageStorage")
-    .info(`maps/${worldUUID}/${mapId}.jpg`)
+    .info(`maps/${worldUUID}/${mapId}`)
   if (metadataError) {
     throw new Error(metadataError.message)
   }
-  // if (!metadata) {
-  //   return null
-  // }
-  // const meta = metadata.metadata as { user_metadata?: { imageWidth: number; imageHeight: number } }
-  // if (!meta.user_metadata) {
-  //   throw new Error("Wriong meta")
-  // }
-  // const { imageWidth, imageHeight } = meta.user_metadata
-  // if (!imageWidth || !imageHeight) {
-  //   throw new Error("Wriong meta")
-  // }
+  if (!metadata) {
+    return null
+  }
+  if (!metadata.metadata) {
+    throw new Error("Wriong meta")
+  }
+  const meta = metadata.metadata as unknown as { imageWidth: number; imageHeight: number }
+  const { imageWidth, imageHeight } = meta
+  if (!imageWidth || !imageHeight) {
+    throw new Error("Wriong meta")
+  }
   const { data: mapImageURL } = await supabase.storage
     .from("ImageStorage")
     .createSignedUrl(`maps/${worldUUID}/${mapId}`, 60)
-  // return { mapImageURL, imageWidth, imageHeight }
-  return { mapImageURL: mapImageURL?.signedUrl, imageWidth: 2000, imageHeight: 2000 }
+  // console.log(mapImageURL)
+  return { mapImageURL: mapImageURL?.signedUrl, imageWidth, imageHeight }
+  // return { mapImageURL: mapImageURL?.signedUrl, imageWidth: 2000, imageHeight: 2000 }
 }
 
 export async function fetchTokenData(tokenId: number) {
