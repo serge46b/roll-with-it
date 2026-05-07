e66541e6ac0b0850f3d1b79a46f097c1dc5bd771
Nastya <eshsen@users.noreply.github.com>
2026-05-07 12:01:01 +0700

    Updated db schemas and added avatar fetching

--- src/modules/MapManager/api/fetchers.ts
+++ src/modules/MapManager/api/fetchers.ts
@@ -1,7 +1,6 @@
 "use server"
 
 import { createClient } from "@/shared/supabase/server"
-import { Character } from "@/shared/types/dbSchema"
 
 export async function fetchMapImage(worldUUID: string, mapId: number) {
   const supabase = await createClient()
@@ -15,7 +14,7 @@ export async function fetchMapImage(worldUUID: string, mapId: number) {
     return null
   }
   if (!metadata.metadata) {
-    throw new Error("Wriong meta")
+    throw new Error("Empty meta")
   }
   const meta = metadata.metadata as unknown as { imageWidth: number; imageHeight: number }
   const { imageWidth, imageHeight } = meta
@@ -25,19 +24,37 @@ export async function fetchMapImage(worldUUID: string, mapId: number) {
   const { data: mapImageURL } = await supabase.storage
     .from("ImageStorage")
     .createSignedUrl(`maps/${worldUUID}/${mapId}`, 60)
-  // console.log(mapImageURL)
   return { mapImageURL: mapImageURL?.signedUrl, imageWidth, imageHeight }
-  // return { mapImageURL: mapImageURL?.signedUrl, imageWidth: 2000, imageHeight: 2000 }
+}
+
+export async function fetchTokenImage(tokenId: number) {
+  const supabase = await createClient()
+  const { data: user } = await supabase.auth.getUser()
+  const userId = user?.user?.id
+  if (!userId) {
+    return
+  }
+  const { data, error } = await supabase.storage
+    .from("ImageStorage")
+    .createSignedUrl(`avatars/${userId}/${tokenId}`, 60)
+  if (error) {
+    throw new Error(error.message)
+  }
+  return data?.signedUrl
+}
+
+export async function fetchMapData(worldUUID: string, mapId: number) {
+  const supabase = await createClient()
+  const { data, error } = await supabase.from("map").select("*").eq("world", worldUUID).eq("id", mapId).single()
+  if (error) {
+    throw new Error(error.message)
+  }
+  return data
 }
 
 export async function fetchTokenData(tokenId: number) {
   const supabase = await createClient()
-  const { data, error } = await supabase
-    .from("character")
-    .select("*")
-    .eq("id", tokenId)
-    .single()
-    .overrideTypes<Character>()
+  const { data, error } = await supabase.from("character").select("*").eq("id", tokenId).single()
   if (error) {
     throw new Error(error.message)
   }
