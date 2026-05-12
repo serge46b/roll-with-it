5d35bf8f5f26704be17ffd73d5d7a8e499956d88
Asya <aneXXa@users.noreply.github.com>
2026-05-12 12:01:21 +0700

    Small fixes performed and tmp pages pages added

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,10 @@
+import { TopBar } from "@/modules/Topbar"
+
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <>
+      <TopBar />
+      <main>{children}</main>
+    </>
+  )
 }
