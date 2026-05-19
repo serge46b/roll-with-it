76d31afe9a84b1432e5969bf64538c1224446c8b
Asya <aneXXa@users.noreply.github.com>
2026-05-19 12:01:48 +0700

    Added world connection logic

--- src/modules/MapManager/components/MapEdit/MiniMapField.tsx
+++ src/modules/MapManager/components/MapEdit/MiniMapField.tsx
@@ -5,19 +5,19 @@ import { MapEditModalContext } from "./MapEditModalContext"
 import { useContext, useTransition } from "react"
 import twclsx from "@/shared/utils/twClassMerge"
 import { Tables } from "@/shared/supabase/dbSchema"
+import { usePathname, useRouter } from "next/navigation"
 
 export default function MiniMapField({ mapImage, map }: { mapImage: MapImage; map: Tables<"map"> }) {
   const { openModal } = useContext(MapEditModalContext)
   const [isOpenModalPending, startOpenModalTransition] = useTransition()
+  const pathname = usePathname()
+  const router = useRouter()
   if (!map) return <></>
   return (
-    <article className="relative w-full overflow-hidden rounded-[0.5em] border border-white/25 bg-black/40 text-[length:1em] text-white">
-      <p className="relative z-10 truncate bg-black/75 px-[0.75em] py-[0.55em] text-[0.9em] font-medium tracking-wide text-white">
-        {map.name}
-      </p>
+    <div className="relative w-full">
       <button
         className={twclsx(
-          "absolute top-[0.5em] right-[0.25em] z-20 rounded-md border border-white/30 bg-black/60 px-[0.55em] py-[0.25em] text-[0.72em] font-medium text-white transition-colors hover:bg-black/75",
+          "absolute top-1 right-1 rounded-md bg-white/50 p-2 text-black",
           isOpenModalPending && "animate-pulse",
         )}
         onClick={() => {
@@ -28,16 +28,19 @@ export default function MiniMapField({ mapImage, map }: { mapImage: MapImage; ma
       >
         Edit Map
       </button>
-      <div className="relative aspect-[2/1] w-full bg-white/10">
-        <Image
-          src={mapImage.mapImageURL}
-          alt={`Карта '${map.name}'`}
-          fill
-          sizes="100vw"
-          className="object-cover"
-          unoptimized
-        />
-      </div>
-    </article>
+      <p className="absolute top-1 left-1 text-white">{map.name}</p>
+      <Image
+        src={mapImage.mapImageURL}
+        alt={`Карта '${map.name}'`}
+        width={mapImage.imageWidth}
+        height={mapImage.imageHeight}
+        sizes="20vw"
+        className="h-auto w-full object-contain"
+        unoptimized
+        onClick={() => {
+          router.push(`${pathname}?mapId=${map.id}`)
+        }}
+      />
+    </div>
   )
 }
