c32bb8ec2fca89017064f1beacb664e18b12d973
Asya <aneXXa@users.noreply.github.com>
2026-05-21 12:01:54 +0700

    Fixes of bugs in PlayerDataManager

--- src/modules/MapManager/components/MapsList.tsx
+++ src/modules/MapManager/components/MapsList.tsx
@@ -1,15 +1,18 @@
 import { fetchAllMaps } from "../api/fetchers"
 import AddMapField from "./MapEdit/AddMapField"
+import MapEditModalContextProvider from "./MapEdit/MapEditModalContext"
 import MiniMapField from "./MapEdit/MiniMapField"
 
 export default async function MapsList({ worldUUID }: { worldUUID: string }) {
   const maps = await fetchAllMaps(worldUUID)
   return (
     <div className="flex flex-col gap-4">
-      {maps.map((map) => (
-        <MiniMapField key={map.map.id} mapImage={map.mapImage} map={map.map} />
-      ))}
-      <AddMapField key={maps.length} />
+      <MapEditModalContextProvider>
+        {maps.map((map) => (
+          <MiniMapField key={map.map.id} mapImage={map.mapImage} map={map.map} />
+        ))}
+        <AddMapField key={maps.length} />
+      </MapEditModalContextProvider>
     </div>
   )
 }
