a955e838a27a47b98992072a132356b39ad05314
Solly <SollyMor@users.noreply.github.com>
2026-05-22 12:01:57 +0700

    Fixes of bugs in PlayerDataManager

---
+++ src/modules/Dice/components/RollDiceModalContext.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { createContext, useState } from "react"
+import { Dialog } from "@base-ui/react/dialog"
+import { StyledModal } from "@/components/StyledModal"
+import Image from "next/image"
+
+interface RollDiceModalContextInterface {
+  openModal: (sides: number, value: number) => void
+  closeModal: () => void
+}
+
+export const RollDiceModalContext = createContext<RollDiceModalContextInterface>({
+  openModal: () => {},
+  closeModal: () => {},
+})
+
+const RollDiceDialogHandler = Dialog.createHandle()
+
+export function RollDiceModalContextProvider({ children }: { children: React.ReactNode }) {
+  const openModal = (sides: number, value: number) => {
+    console.log(`d${sides}: ${value}`)
+    setDiceSides(sides)
+    setDiceValue(value)
+    RollDiceDialogHandler.open("context-trigger")
+  }
+  const closeModal = () => {
+    RollDiceDialogHandler.close()
+  }
+  const [diceSides, setDiceSides] = useState(0)
+  const [diceValue, setDiceValue] = useState(0)
+  return (
+    <>
+      <RollDiceModalContext.Provider value={{ openModal, closeModal }}>{children}</RollDiceModalContext.Provider>
+      <StyledModal handle={RollDiceDialogHandler}>
+        <div className="flex flex-col gap-4 p-8">
+          <div className="flex h-[30vh] w-[40vw] flex-col items-center justify-center gap-5 border border-white text-xl">
+            <Image src={`/svgs/dice${diceSides}.svg`} alt={`d${diceSides}`} width={60} height={60} />
+            {`Результат броска d${diceSides}: ${diceValue}!`}
+          </div>
+          <div className="flex w-full justify-center">
+            <button onClick={closeModal}>Закрыть</button>
+          </div>
+        </div>
+      </StyledModal>
+    </>
+  )
+}
