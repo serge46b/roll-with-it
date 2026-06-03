a975e360185e7c31e5136f7e05621ee69394e7fb
aneXXa <an_na_rib@mail.ru>
2026-06-03 14:19:45 +0700

    Remove unused login-related components to fix dependances

--- src/app/(login-bad)/layout.tsx
+++
@@ -1,7 +0,0 @@
-// TODO: Refactor all login pages, main logic should be in modules
-
-export default function LoginLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="login-stars flex h-screen flex-col items-center justify-center overflow-hidden">{children}</main>
-  )
-}
